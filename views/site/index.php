a95b6f1f42a0b35754b151a2515fe18302fae7d3
Innosan <reau.beu@gmail.com>
2022-11-09 14:34:16 +0300

    basic structure

--- views/site/index.php
+++ views/site/index.php
@@ -2,51 +2,62 @@
 
 /** @var yii\web\View $this */
 
-$this->title = 'My Yii Application';
+$this->title = 'Foodie';
 ?>
 <div class="site-index">
 
-    <div class="jumbotron text-center bg-transparent">
-        <h1 class="display-4">Congratulations!</h1>
-
-        <p class="lead">You have successfully created your Yii-powered application.</p>
-
-        <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>
+    <div class="d-flex justify-content-between mt-4">
+        <p class="greeting">Ну привет,<br>Пипа!</p>
+        <div class="rating-container">
+            <p class="rating-heading mb-2">
+                Мой Рейтинг
+            </p>
+            <p class="rating-value glitch">
+                <span aria-hidden="true">ультрамегахорош</span>
+                ультрамегахорош
+                <span aria-hidden="true">ультрамегахорош</span>
+            </p>
+        </div>
     </div>
 
-    <div class="body-content">
+    <div class="body-content w-75 m-auto d-flex justify-content-between">
 
-        <div class="row">
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-outline-secondary" href="http://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>
+        <div class="trackers-container d-flex">
+            <div class="tracker-card tall">
+                <img class="card-icon" src=""/>
+                <p class="card-title">Вес</p>
+                <img class="card-subicon" src=""/>
+                <p class="card-content">240 кг.,<br>сбросить бы</p>
             </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-outline-secondary" href="http://www.yiiframework.com/forum/">Yii Forum &raquo;</a></p>
+            <div class="tracker-cards-container d-flex flex-column justify-content-between">
+                <div class="tracker-card wide">
+                    <img class="card-icon" src=""/>
+                    <p class="card-title">Сон</p>
+                    <p class="card-content">4 часа за сегодня,<br>довольно мало</p>
+                </div>
+                <div class="tracker-card wide">
+                    <img class="card-icon" src=""/>
+                    <p class="card-title">Водичка</p>
+                    <p class="card-content">3 стакана<br>700ml.</p>
+                </div>
             </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-outline-secondary" href="http://www.yiiframework.com/extensions/">Yii Extensions &raquo;</a></p>
+            <div class="tracker-card ultrawide mt-4">
+                <img class="card-icon" src=""/>
+                <p class="card-title">Хавчик</p>
+                <div class="input-group">
+                    <input type="range">
+                    <input type="range">
+                    <input type="range">
+                    <input type="range">
+                    <input type="range">
+                </div>
             </div>
+        </div>
+        <div class="daily-tasks">
+            <label for="taskCheckbox">
+                покушал утром?
+                <input name="task" id="taskCheckbox" type="checkbox">
+            </label>
         </div>
 
     </div>
